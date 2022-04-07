9d6fe1918cab58f7c81d4ab080e88fce674be807
Jasper Meggitt <jasper.meggitt@gmail.com>
2022-04-07 16:02:25 -0400

    Switch from gcc to cc

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
+use cc::Build;
+
 fn main() {
-    #[cfg(target_os = "linux")]
-    gcc::compile_library("libgc_clib_x64.a", &["src/heap/gc/clib_x64.c"]);
+    Build::new()
+        .file("src/heap/gc/clib_x64.c")
+        .compile("gc_clib_x64");
+
+    println!("cargo:rerun-if-changed=src/heap/gc/clib_x64.c");
 
     // if cfg!(target_os = "linux") {
     //     cc::Config::new()
