1f67c390008c793932739d6b5e05b024d3c44799
Jasper Meggitt <jasper.meggitt@gmail.com>
2022-04-07 14:58:25 -0400

    cargo fmt project

--- build.rs
+++ build.rs
@@ -1,13 +1,12 @@
-
 fn main() {
     #[cfg(target_os = "linux")]
     gcc::compile_library("libgc_clib_x64.a", &["src/heap/gc/clib_x64.c"]);
 
-   // if cfg!(target_os = "linux") {
-   //     cc::Config::new()
-   //                  .flag("-lpfm")
-   //                  .flag("-O3")
-   //                  .file("src/common/perf.c")
-   //                  .compile("libgc_perf.a");
-   // }
+    // if cfg!(target_os = "linux") {
+    //     cc::Config::new()
+    //                  .flag("-lpfm")
+    //                  .flag("-O3")
+    //                  .file("src/common/perf.c")
+    //                  .compile("libgc_perf.a");
+    // }
 }
